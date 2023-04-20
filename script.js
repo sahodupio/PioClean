b028ce94d3d70afe838ac338735700f5ecdab0d2
sahodupio <113264470+sahodupio@users.noreply.github.com>
2023-04-20 08:48:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var name = prompt("What is your name");
+
+
+
+alert ("Hello World");
+
+prompt ("Where are you login from");
+
+var tweet = prompt("Compose your tweet");
+var tweetCount = tweet.length;
+alert("you have written" + tweetCount + "Characters, you have" + (140 - tweetCount) + "character remaining");
